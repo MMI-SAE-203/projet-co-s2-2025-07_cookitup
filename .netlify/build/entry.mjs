ced78057658c024d73fdb5f8d385c51369e87c8d
winkel-titouan <titouan.winkel@edu.univ-fcomte.fr>
2025-06-05 11:06:01 +0200

    test

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_Pkzrww_S.mjs';
+import { manifest } from './manifest_UY3AAJKi.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -64,7 +64,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "4b655f34-4bb1-446f-b405-841641a87c6c"
+    "middlewareSecret": "c54d26c1-c538-4798-b9cc-66ceb8eb946f"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
